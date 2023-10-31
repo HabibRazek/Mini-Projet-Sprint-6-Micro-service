cadf0dd32b281a9771954ccff4b2fe70fb3ea4da
HabibRazek <108337082+HabibRazek@users.noreply.github.com>
2023-10-31 15:47:26 +0100

    Add files via upload

---
+++ src/main/java/com/habib/sprint5/service/UserService.java
@@ -0,0 +1,23 @@
+package com.habib.sprint5.service;
+
+import com.habib.sprint5.entities.Role;
+import com.habib.sprint5.entities.User;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+    User findUserByUsername (String username);
+    Role addRole(Role role);
+    User addRoleToUser(long id, Role r);
+    List<User> findAllUsers();
+
+    void deleteUser(long id);
+    User removeRoleFromUser(long id, Role r);
+    List<Role> findAllRoles();
+
+    Role findRoleById(Long id);
+
+    User findUserById(Long id);
+
+}
